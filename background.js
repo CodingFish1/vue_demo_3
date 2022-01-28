ab4eb99817653e8a1780784c716e6e8be75e909d
CodingFisher <cppmaster@outlook.com>
2022-01-28 22:56:38 +1030

    first time

---
+++ background.js
@@ -0,0 +1,15 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+const apiUrl='https://vue3-course-api.hexschool.io/v2';
+const path='williamone';
+
+const app=Vue.createApp({
+    data(){
+        return {
+            text:"lol"
+        }
+    }
+
+
+})
+
+app.mount('#app');
